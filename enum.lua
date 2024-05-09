6bb1b7ccffb65cea4d121695affe547f5ed56eb0
Alexandru Barbur <alex@ctrlc.name>
2024-05-08 20:18:52 -0400

    Refactor Enum internal implementation into a top-level table.

--- enum.lua
+++ enum.lua
@@ -88,6 +88,92 @@ local enum_metatable <const> = {}
 local enum_private <const> = setmetatable({}, {__mode='k'})
 local enum_reserved_keys <const> = {['has']=true}
 
+-- private implementation
+local function enum_internal_has (self, symbol)
+  local private <const> = assert(enum_private[self], "Enum instance not recognized: " .. tostring(self))
+  assert(Symbol.is(symbol), "symbol parameter must be a Symbol instance")
+  return private.symbols_by_name[symbol.name] == symbol
+end
+
+local enum_internal_metatable <const> = {
+  __name = 'Enum',
+  __metatable = enum_metatable,
+  __len = function (self)
+    local private <const> = assert(enum_private[self], "Enum instance not recognized: " .. tostring(self))
+
+    -- expose symbols as dense array to support ipairs() enumeration
+    return #private.symbols
+  end,
+  __index = function (self, key)
+    local private <const> = assert(enum_private[self], "Enum instance not recognized: " .. tostring(self))
+
+    -- export symbols as dense array to support ipairs() enumeration
+    if math.type(key) == 'integer' then
+      return private.symbols[key]
+    -- enum symbol membership test
+    elseif key == 'has' then
+      return enum_internal_has
+    -- export symbols as map with names as keys
+    else
+      assert(enum_reserved_keys[key] == nil, "failed to handle all reserved keys")
+      return private.symbols_by_name[key]
+    end
+  end,
+  __newindex = function (_, _, _)
+    error("Enum definition cannot be modified")
+  end,
+  __pairs = function (self)
+    local private <const> = assert(enum_private[self], "Enum instance not recognized: " .. tostring(self))
+
+    -- note: lua table iteration is in arbitrary order whereas this always
+    -- iterates in the same order which is technically backwards compatible
+    -- for loops: https://www.lua.org/manual/5.4/manual.html#3.3.5
+    local function iterate(names, name) -- state variable, initial or previous control value
+      -- note: not strictly necessary as table.remove({}, 1) and inner[nil]
+      -- both return nil so the loop ends on it's own but this is safer
+      if #names == 0 then
+        return
+      end
+
+      local name <const> = table.remove(names, 1)
+      local value <const> = private.symbols_by_name[name].value
+      return name, value -- control value, remaining loop values
+    end
+
+    local names <const> = {}
+    for name, _ in pairs(private.symbols_by_name) do
+      table.insert(names, name)
+    end
+
+    -- iterator function, state variable, initial control value, closing variable
+    return iterate, names, names[1], nil
+  end,
+  __call = function (self, value)
+    local private <const> = assert(enum_private[self], "Enum instance not recognized: " .. tostring(self))
+
+    -- look up symbols by value
+    return private.symbols_by_value[value]
+  end,
+  __eq = function (a, b)
+    -- note: implicitly verifies both values are symbol instances since at
+    -- least one of them must be for this function to be called
+    if getmetatable(a) ~= getmetatable(b) then
+      return false
+    end
+
+    -- retrieve instance private data
+    local pa <const> = assert(enum_private[a], "Enum instance not recognized: " .. tostring(a))
+    local pb <const> = assert(enum_private[b], "Enum instance not recognized: " .. tostring(b))
+
+    -- must be the same instance but we compare private data tables to
+    -- avoid a stack overflow when comparing instance tables (ex: a == b)
+    return (pa == pb)
+  end,
+  __gc = function (self)
+    enum_private[self] = nil
+  end
+}
+
 -- public interface
 local Enum <const> = setmetatable({
   create = function (symbol_data)
@@ -159,81 +245,7 @@ local Enum <const> = setmetatable({
       symbols_by_value=symbols_by_value
     }
 
-    return setmetatable(instance, {
-      __name = 'Enum',
-      __metatable = enum_metatable,
-      __len = function (_)
-        -- expose symbols as dense array to support ipairs() enumeration
-        return #symbols
-      end,
-      __index = function (_, key)
-        -- export symbols as dense array to support ipairs() enumeration
-        if math.type(key) == 'integer' then
-          return symbols[key]
-        -- enum symbol membership test
-        elseif key == 'has' then
-          return function (_, symbol)
-            -- XXX: actually use omitted self value above instead of assuming
-            -- the method is called on this instance
-            assert(Symbol.is(symbol), "symbol parameter must be a Symbol instance")
-            return symbols_by_name[symbol.name] == symbol
-          end
-        -- export symbols as map with names as keys
-        else
-          assert(enum_reserved_keys[key] == nil, "failed to handle all reserved keys")
-          return symbols_by_name[key]
-        end
-      end,
-      __newindex = function (_, _, _)
-        error("Enum definition cannot be modified")
-      end,
-      __pairs = function (_)
-        -- note: lua table iteration is in arbitrary order whereas this always
-        -- iterates in the same order which is technically backwards compatible
-        -- for loops: https://www.lua.org/manual/5.4/manual.html#3.3.5
-        local function iterate(names, name) -- state variable, initial or previous control value
-          -- note: not strictly necessary as table.remove({}, 1) and inner[nil]
-          -- both return nil so the loop ends on it's own but this is safer
-          if #names == 0 then
-            return
-          end
-
-          local name <const> = table.remove(names, 1)
-          local value <const> = symbols_by_name[name].value
-          return name, value -- control value, remaining loop values
-        end
-
-        local names <const> = {}
-        for name, _ in pairs(symbols_by_name) do
-          table.insert(names, name)
-        end
-
-        -- iterator function, state variable, initial control value, closing variable
-        return iterate, names, names[1], nil
-      end,
-      __call = function (_, value)
-        -- look up symbols by value
-        return symbols_by_value[value]
-      end,
-      __eq = function (a, b)
-        -- note: implicitly verifies both values are symbol instances since at
-        -- least one of them must be for this function to be called
-        if getmetatable(a) ~= getmetatable(b) then
-          return false
-        end
-
-        -- retrieve instance private data
-        local pa <const> = assert(enum_private[a], "Enum instance not recognized: " .. tostring(a))
-        local pb <const> = assert(enum_private[b], "Enum instance not recognized: " .. tostring(b))
-
-        -- must be the same instance but we compare private data tables to
-        -- avoid a stack overflow when comparing instance tables (ex: a == b)
-        return (pa == pb)
-      end,
-      __gc = function (_)
-        enum_private[instance] = nil
-      end
-    })
+    return setmetatable(instance, enum_internal_metatable)
   end,
   is = function (value)
     return (getmetatable(value) == enum_metatable)
